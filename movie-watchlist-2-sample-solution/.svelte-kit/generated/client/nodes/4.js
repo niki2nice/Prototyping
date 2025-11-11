5b8577359aa2241f2162b10333b1cd96e5e475b9
niki2nice <aleksnik@students.zhaw.ch>
2025-11-11 23:25:02 +0100

    first commit

---
+++ movie-watchlist-2-sample-solution/.svelte-kit/generated/client/nodes/4.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/movies/[movie_id]/+page.svelte";
---
+++ movie-watchlist-2-sample-solution/.svelte-kit/generated/client/nodes/5.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/movies/create/+page.svelte";
